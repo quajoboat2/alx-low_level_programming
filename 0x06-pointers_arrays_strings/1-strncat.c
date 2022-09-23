67798fd5c441c12297addf0ea0e596d8c63d5749
quajoboat2 <quajogyamfi@outlook.com>
2022-09-23 10:08:49 -0700

    Write a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,9 +9,9 @@
  * Return: a pointer to the resulting string dest
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i ,j;
+	int i, j;
 
 	i = 0;
 	j = 0;
