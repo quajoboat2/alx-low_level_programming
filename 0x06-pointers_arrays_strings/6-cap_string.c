62e54b4bc1bd7721e0b94e8e75d7a08daec98e2e
Adu Boateng Richard <quajogyamfi@outlook.com>
2022-09-23 22:28:32 +0000

    Write a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,30 +5,37 @@
  *
  * @s: s is the world to be analyzed
  *
- * Return: Always 0.
+ * Return: string
  */
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 1, j, check;
+char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
+
+	if (s[0] > 96 && s[0] < 123)
+		s[0] -= 32;
 
 	while (s[i] != '\0')
 	{
-		if (s[0] <= 122 && s[0] >= 97)
+		if (s[i] > 96 && s[i] < 123)
 		{
-			s[0] = s[0] - 32;
-		}
-		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
-		    s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
-		    	s[i] == '!' || s[i] == '?' || s[i] == '(' ||
-			s[i] == ')' || s[i] == '{' || s[i] == '}')
-		{
-			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+			j = 0;
+			check = 0;
+			while (check == 0 && j < 13)
 			{
-				s[i + 1] = s[i + 1] - 32;
+				if (s[i - 1] == a[j])
+				{
+					check = 1;
+				}
+				j++;
+			}
+			if (check == 1)
+			{
+				s[i] -= 32;
 			}
 		}
-	i++;
+		i++;
 	}
-	return (s);
+return (s);
 }
