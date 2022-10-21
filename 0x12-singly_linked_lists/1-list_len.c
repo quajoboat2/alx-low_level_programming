cce9831fd07b3884abd0d04530cd9ba6d5488c51
Adu Boateng Richard <quajogyamfi@outlook.com>
2022-10-21 07:07:13 +0000

    task 1 commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,18 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
  *
- * Return: number of elements in h
+ * Return: number of elements in the linked list
  */
-
 size_t list_len(const list_t *h)
 {
-	size_t list_len(const list_t *h)
-{
-	int nodes = 0;
+	const list_t *tmp;
+	unsigned int i;
 
-	while (h)
-	{
-		nodes += 1;
-		h = h->next;
-	}
-	return (nodes);
+	tmp = h;
+	for (i = 0; tmp; i++)
+		tmp = tmp->next;
+	return (i);
 }
