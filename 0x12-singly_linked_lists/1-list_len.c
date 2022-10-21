b8d9f7ed97a7bd6e4a65be9d65b45adde5abc34c
Adu Boateng Richard <quajogyamfi@outlook.com>
2022-10-21 17:44:09 +0000

    updated commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,21 @@
 #include "lists.h"
-
 /**
- * list_len - returns the number of elements in a linked list_t list
- * @h: the linked list to check
- * Return: number of elements in @h
- */
-
+* list_len - Returns the number of elements in a linked `list_t` list
+* @h: head of linked list
+* Return: number of elements in list
+*/
 size_t list_len(const list_t *h)
 {
-	int counter = 0;
+	const list_t *temp;
+	size_t c;
 
-	while (h)
+	c = 0;
+	temp = h;
+	while (temp != NULL)
 	{
-		h = h->next;
-		counter++;
+		temp = temp->next;
+		c++;
 	}
-	return (counter);
+
+	return (c);
 }
